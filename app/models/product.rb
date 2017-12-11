cde6758e1b7dd709668488a8cdc2b14ead90db71
Luciano Oliveira <luciano101@msn.com>
2017-12-11 13:39:57 -0800

    add image to db seed

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,14 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
+
+  attr_reader :image_remote_url
   
   has_attached_file :product_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\z/
+
+  def image_remote_url=(url)
+    self.image = URI.parse(url)
+    @image_remote_url = url
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,43 @@
-class Seed
+# class Seed
 
-  def self.begin
-    seed = Seed.new
-    seed.generate_products
-  end
+#   def self.begin
+#     seed = Seed.new
+#     seed.generate_products
+#   end
 
-  def generate_products
-    20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
-    end
-  end
-end
+#   def generate_products
+#     4.times do |i|
+#       Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+#     end
+#   end
+# end
 
 
-Seed.begin
+# Seed.begin
+
+Product.create!([
+  {
+    name: 'Bananas',
+    product_img: URI.parse('https://i.imgur.com/6tGwubv.jpg'),
+    price: rand(5...14),
+    description: Faker::Lorem.sentence(5, false, 0).chop
+  },
+  {
+    name: 'Acai',
+    product_img: URI.parse('https://i.imgur.com/xzetOSU.jpg'),
+    price: rand(5...14),
+    description: Faker::Lorem.sentence(5, false, 0).chop
+  },
+  {
+    name: 'Apples',
+    product_img: URI.parse('https://i.imgur.com/5hGbyJO.jpg'),
+    price: rand(5...14),
+    description: Faker::Lorem.sentence(5, false, 0).chop
+  },
+  {
+    name: 'Mango',
+    product_img: URI.parse('https://i.imgur.com/iad7hRk.jpg'),
+    price: rand(5...14),
+    description: Faker::Lorem.sentence(5, false, 0).chop
+  }
+])
